ed128765da9ef0fd1c71bd735b1088687506b228
canyonbryson <58871744+canyonbryson@users.noreply.github.com>
2020-03-04 11:32:09 -0700

    Update chemostat.php

--- chemostat.php
+++ chemostat.php
@@ -1,16 +1,13 @@
-
-
 <?php
+
+from chemostat_code.py import OD
+
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-temp = current data from raspberry pi;
-
-we may need a separate file for each variable
-OD = data;
-sparging = data;
+OD = OD();
 
 
-echo "temp: {temp}\n\n";
+echo "OD: {OD}\n\n";
 flush();
 ?>
