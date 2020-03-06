cfafa094ce8b8cbf18f9fa69acb7f71196c36c1c
canyonbryson <58871744+canyonbryson@users.noreply.github.com>
2020-03-06 09:19:36 -0700

    Update chemostat.php

--- chemostat.php
+++ chemostat.php
@@ -5,9 +5,9 @@ from chemostat_code.py import OD
 header('Content-Type: text/event-stream');
 header('Cache-Control: no-cache');
 
-OD = OD();
+$OD = OD();
 
 
-echo "OD: {OD}\n\n";
+echo "OD: {$OD}\n\n";
 flush();
 ?>
