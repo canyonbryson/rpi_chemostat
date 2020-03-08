41c17002faad2a2e3887c68540b246de030659ef
canyonbryson <58871744+canyonbryson@users.noreply.github.com>
2020-03-07 19:55:49 -0700

    Update json.py

--- json.py
+++ json.py
@@ -1,14 +1,14 @@
 import json
-from chemostat_code import OD
-from Flask_example import sparging, temp
+from chemostat_code import OD, read_temp
+from Flask_example import sparging
 
 with open('chemostat.json') as jsonFile:
   data = json.load(jsonFile)
   
 for data in data['data']:
   data['OD'] = OD()
-  data['sparging'] = sparging()
-  data['temp'] = temp()
+  #data['sparging'] = sparging()
+  data['temp'] = read_temp()
  
  with open('chemostat.json', 'w') as jsonFIle:
   json.dump(data, jsonFile, indent=2)
