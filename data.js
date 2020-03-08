1369694e00234aac8f3c6d9873bf5b3009ae620e
canyonbryson <58871744+canyonbryson@users.noreply.github.com>
2020-03-07 19:56:40 -0700

    Update data.js

--- data.js
+++ data.js
@@ -9,16 +9,16 @@ function startTime() {
 	document.getElementById('time').innerHTML = "Current Time: " + h + ":" + m + ":" + s;	
 }
 setInterval(startTime, 1000);
-/*
+
 var data = import("data.json");
 var data = JSON.parse(data); */
 /* now data = { data: [OD: "OD", sparging:"sparging", temp: "temp"] };*/
-/*
+
 function displayData() {
   document.getElementById('OD').innerHTML = data.data['OD'];
-  document.getElementById('sparging').innerHTML = data.data['sparging'];
+  /*document.getElementById('sparging').innerHTML = data.data['sparging'];*/
   document.getElementById('temp').innerHTML = data.data['temp'];
 }
-setInteval(displayData, 1000); */
+setInteval(displayData, 1000); 
 
  
