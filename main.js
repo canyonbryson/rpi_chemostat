73cd6e1486056464b3a4a48837bbb463143e1fc7
canyonbryson <58871744+canyonbryson@users.noreply.github.com>
2020-05-14 17:36:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ function startTime() {
 }
 setInterval(startTime, 1000 * 30);
 
- 
+ <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js" integrity="sha256-yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I=" crossorigin="anonymous"></script>
